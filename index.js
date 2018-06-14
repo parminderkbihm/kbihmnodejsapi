891ce401786bf484f68ebb9d08d05bac60c453ad
Parminder Singh <parminder.s@kbihm.com>
2018-06-14 14:53:11 +0530

    initial

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World! OK');
-    console.log('hello world');
-}).listen(8080);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
